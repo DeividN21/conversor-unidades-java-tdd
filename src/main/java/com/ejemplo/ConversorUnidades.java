5868b2d01e368d5844f73d244049c9880c6d80f2
DeividN21 <145998414+DeividN21@users.noreply.github.com>
2025-06-02 17:45:36 +0000

    Correcciones estilo de código (Checkstyle)

--- src/main/java/com/ejemplo/ConversorUnidades.java
+++ src/main/java/com/ejemplo/ConversorUnidades.java
@@ -1,19 +1,22 @@
 package com.ejemplo;
 
+/**
+ * Clase para realizar conversiones entre unidades métricas.
+ */
 public class ConversorUnidades {
-    public static double metrosACentimetros(double metros) {
-        return metros * 100; 
-    }
+  public static double metrosACentimetros(double metros) {
+      return metros * 100; 
+  }
 
-    public static double metrosAMilimetros(double metros) {
-    return metros * 1000;
-    }
+  public static double metrosAMilimetros(double metros) {
+      return metros * 1000;
+  }
 
-    public static double centimetrosAMilimetros(double centimetros) {
-    return centimetros * 10;
-    }
+  public static double centimetrosAMilimetros(double centimetros) {
+      return centimetros * 10;
+  }
 
-    public static double milimetrosAMetros(double milimetros) {
-    return milimetros / 1000;
-    }
+  public static double milimetrosAMetros(double milimetros) {
+      return milimetros / 1000;
+  }
 }
